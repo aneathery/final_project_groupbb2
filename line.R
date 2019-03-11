d9eef9326615dd3a21c24389cf587296e6d0383f
your-full-name <patrickly5@yahoo.com>
2019-03-10 17:03:41 -0700

    I got the line graph to work but I don't know how

--- line.R
+++ line.R
@@ -15,10 +15,19 @@ yearly_gas_data <- gas_data %>%
     year = as.numeric(substr(gas_data$Open.Date, nchar(gas_data$Open.Date) -
     3, nchar(gas_data$Open.Date)))
   ) %>%
-  filter( # Select which fuel type to use
-    Fuel.Type.Code == "BD"
+  filter( 
+    Fuel.Type.Code == "BD" # Select which fuel type to use
   ) %>% 
   group_by(year) %>%
+  count()
+
+# Grouping by fuel type, state, and year
+testing <- gas_data %>%
+  mutate(
+    year = as.numeric(substr(gas_data$Open.Date, nchar(gas_data$Open.Date) -
+      3, nchar(gas_data$Open.Date))) 
+  ) %>% 
+  group_by(Fuel.Type.Code, State, year) %>%
   count()
 
 # Removing the row with NA values
@@ -29,11 +38,12 @@ plot <- plot_ly(
   data = yearly_gas_data,
   x = ~year,
   y = ~n,
-  type = 'bar',
-  mode = "markers",
-  name = "test"
+  type = 'scatter',
+  mode = "line"
 ) 
 
+# Testing line plot out
+yeet <- pull(yearly_gas_data[1,2])
 testplot <- plot_ly(
   data = yearly_gas_data,
   x = ~year,
@@ -41,6 +51,32 @@ testplot <- plot_ly(
   mode = "line+markers"
 ) %>%
   add_lines(y = ~n, name = "linear", line = list(shape = "linear"))
+
+############################## Testing #########################################
+
+bee <- testing %>%
+  filter(Fuel.Type.Code == "BD")
+
+testplot <- plot_ly(
+  data = yearly_gas_data, 
+  x = ~year, 
+  type = 'scatter', 
+  mode = 'line+markers',
+  y = ~n, 
+  name = 'ELEC', 
+  mode = 'line+markers'
+  ) %>%
+  add_trace(
+    x = ~bee$year,
+    y = ~bee$n, 
+    name = 'BD', 
+    mode = 'line+markers'
+    ) %>%
+  layout(title = paste("Stations Opened By Year"),
+         xaxis = list(title = "Year"),
+         yaxis = list (title = "Stations Opened"))
+
+################################################################################
 
 # Gas types
 gas <- c("BD", "CNG", "E85", "ELEC", "HY", "LNG", "LPG")
