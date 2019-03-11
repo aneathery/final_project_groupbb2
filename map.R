b2e7e400bd8d2b742dba6edf82186418089a70d1
aneathery <aneathery@xevo.com>
2019-03-10 20:46:01 -0700

    Make preliminary map. Closes #1

---
+++ map.R
@@ -0,0 +1,39 @@
+# final-project-groupbb2
+
+# This is the file in which Amber will create a map displaying the 
+# location of alternative fuel stations in the United States. 
+
+################################### Set up ###################################
+# Load necessary libraries
+library("leaflet")
+library("dplyr")
+library("ggplot2")
+library("lubridate")
+library("lintr")
+library("ggmap")
+
+# Load raw data 
+raw_data <- read.csv("alt_fuel_data.csv", stringsAsFactors = FALSE)
+
+############################## Bar Chart #####################################
+
+# This map displays the location of alternative fuel stations in the United 
+# States
+
+color_palette <- colorFactor(
+  c("navy", "forestgreen", "yellow", "green", "blue", "orange", "deepskyblue"),
+  domain = unique(stations_df$Fuel.Type.Code) 
+)
+
+stations_map <- leaflet(data = stations_df) %>%
+  addTiles() %>%
+  addCircles(
+    lat = ~Latitude,
+    lng = ~Longitude,
+    popup = paste0("City: ", stations_df$City,
+                   "; Station Name: ", stations_df$Station.Name,
+                   "; Fuel Type: ", stations_df$Fuel.Type.Code),
+    color = ~color_palette(Fuel.Type.Code)
+  ) %>%
+  setView(-100, 37.8, 3.6)
+
