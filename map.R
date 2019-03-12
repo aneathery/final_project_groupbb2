d740824a8121af6ec8d19bb73b01baffa2126268
aneathery <aneathery@xevo.com>
2019-03-11 23:05:40 -0700

    Add fuel select widget to Chart 1 (map). Closes #3

--- map.R
+++ map.R
@@ -11,36 +11,32 @@ library("ggplot2")
 library("lubridate")
 library("lintr")
 library("ggmap")
-library("styler")
 
 # Load raw data
 raw_data <- read.csv("alt_fuel_data.csv", stringsAsFactors = FALSE)
 
-########################## Map Visualization  ##################################
+############################## Bar Chart #####################################
 
 # This map displays the location of alternative fuel stations in the United
 # States
 
-make_station_map <- function(stations_df, Fuel.Type.Code) {
+make_station_map <- function(stations_df, fuel_choice){
   color_palette <- colorFactor(
-    c(
-      "navy", "deepskyblue", "dodgerblue4", "turquoise4", "blue", "lightblue1",
-      "midnightblue"
-    ),
+    c("navy", "deepskyblue", "dodgerblue4", "turquoise4", "blue", "lightblue1", "midnightblue"),
     domain = unique(stations_df$Fuel.Type.Code)
   )
-
-  stations_map <- leaflet(data = stations_df) %>%
+  
+  fuelType <- filter(stations_df, stations_df$Fuel.Type.Code == fuel_choice)
+  
+  stations_map <- leaflet(data = fuelType) %>%
     addTiles() %>%
     addCircles(
       lat = ~Latitude,
       lng = ~Longitude,
-      popup = paste0(
-        "City: ", stations_df$City,
-        "; Station Name: ", stations_df$Station.Name,
-        "; Fuel Type: ", stations_df$Fuel.Type.Code
-      ),
-      color = ~ color_palette(Fuel.Type.Code)
+      popup = paste0("City: ", stations_df$City,
+                     "; Station Name: ", stations_df$Station.Name,
+                     "; Fuel Type: ", stations_df$Fuel.Type.Code),
+      color = ~color_palette(Fuel.Type.Code)
     ) %>%
     setView(-100, 37.8, 3.6)
 }
